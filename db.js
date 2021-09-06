0853fbb83111c38aa60d8d8ac702e344c491e322
Erick Mulindi <37551785+mulindierick@users.noreply.github.com>
2021-09-07 00:14:32 +0300

    added ideas

---
+++ db.js
@@ -0,0 +1,27 @@
+
+
+// ideas for db design tables 
+
+// - users (
+// user id
+// email
+// paswword
+// username  
+//)
+
+// - campaign (
+// campaign id
+// campaign name
+// campaign dscription
+// campaign owner references user id
+// )
+
+// - donations ( 
+// donation id
+// item name, 
+// campaing id  references compain campaign id
+// quanitity
+// name of person who donated
+// when to deliver
+// where to deliver 
+// )
